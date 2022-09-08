bbaf3d8170691d969b4ceb2f83c0947fe1bc75bd
Rajan Tschan <RTschan@Hamilton.ch>
2022-09-08 15:06:27 +0200

    Add functionality to food-list-component

--- dev/src/app/food-list/food-list.page.ts
+++ dev/src/app/food-list/food-list.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IValues } from '../components/category-item/category-item.component';
 
 @Component({
   selector: 'app-food-list',
   templateUrl: './food-list.page.html',
   styleUrls: ['./food-list.page.scss'],
 })
+
 export class FoodListPage implements OnInit {
+  foods: IValues[];
+  category: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
+    this.category = this.router.getCurrentNavigation().extras.state.categoryName; 
+    this.foods = this.router.getCurrentNavigation().extras.state.values;
+    console.log("food-list-data: ", this.foods);
   }
 
+  ngOnInit() {}
 }
