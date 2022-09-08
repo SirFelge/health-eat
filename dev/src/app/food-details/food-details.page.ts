82d92a92305f212c36d2a1b480ff5602976add45
Rajan Tschan <RTschan@Hamilton.ch>
2022-09-08 16:59:06 +0200

    Add data-fetch-functionality

--- dev/src/app/food-details/food-details.page.ts
+++ dev/src/app/food-details/food-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IValues } from '../components/category-item/category-item.component';
 
 @Component({
   selector: 'app-food-details',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./food-details.page.scss'],
 })
 export class FoodDetailsPage implements OnInit {
+  selectedFood: IValues;
+  healthy: string;
+  energy: string;
+  macros: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
+    this.selectedFood = this.router.getCurrentNavigation().extras.state.selectedValue;
+    this.healthy = this.selectedFood.healthy.toString() + "%";
+    this.energy = this.selectedFood.energy.toString() + "%";
+    this.macros = this.selectedFood.macros.toString() + "%";
+    
+    console.log(this.selectedFood);
   }
 
+  ngOnInit() {}
 }
